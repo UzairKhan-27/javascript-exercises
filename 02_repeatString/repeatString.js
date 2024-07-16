979347b6eef4d706035554c5d0704464984837b6
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-07-17 02:24:55 +0500

    My Solution All Passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+// const repeatString = function("hey",3) {
 
-};
-
+// };
+function repeatString(string,number)
+{
+    if(number<0)
+        return "ERROR";
+    let stringConcat=""
+    for(let i=0;i<number;i++)
+    {
+        stringConcat = stringConcat+string; 
+    }
+    return stringConcat;
+}
+console.log(repeatString("bye",10));
 // Do not edit below this line
 module.exports = repeatString;
