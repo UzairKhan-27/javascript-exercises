159f99efade248c2f624c172ebe067896420d4c0
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-07-17 14:04:35 +0500

    see original solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    // let index=array.indexOf(numberToRemove);
+    for(let i=0;i<array.length;i++)
+    {
+        args.forEach()
+        {
+        if(array[i]===arg)
+        {
+            array.splice(i,1);
+            i--;
+        }
+        }
+    }
+    return array;    
 };
 
 // Do not edit below this line
